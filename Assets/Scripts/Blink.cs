68d19e062a39041dea3977d12899e5a136e78a12
Jesse Salinas <jessesalinas53@yahoo.com>
2021-04-29 13:38:22 -0500

    Level complete, semi-functional blink ability

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    [SerializeField] Camera cameraController;
+    [SerializeField] Transform rayOrigin;
+    [SerializeField] float shootDistance = 30f;
+    [SerializeField] ParticleSystem handParticles;
+    [SerializeField] ParticleSystem castPointParticles;
+    //might not need[SerializeField] ParticleSystem castGroundPointParticles;
+    [SerializeField] ParticleSystem landingParticles;
+    [SerializeField] AudioClip startCastAudio = null;
+    [SerializeField] AudioClip executeCastAudio = null;
+    //isnt in dh[SerializeField] AudioClip landingAudio = null;
+
+    RaycastHit objectHit; // Stores info about raycast hit
+    bool blinkFinished = false;
+
+    private void Update()
+    {
+        while (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCast();
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            ExecuteCast();
+        }
+    }
+
+    void StartCast()
+    {
+        // Calculate direction to shoot the ray
+        Vector3 rayDirection = cameraController.transform.forward;
+        // Cast a debug ray
+        Debug.DrawRay(rayOrigin.position, rayDirection * shootDistance, Color.blue, 1f);
+
+        //handParticles.Play();
+        //castPointParticles.Play();
+        //AudioSource.PlayClipAtPoint(startCastAudio, this.gameObject.transform.position);
+    }
+
+    void ExecuteCast()
+    {
+        //AudioSource.PlayClipAtPoint(executeCastAudio, this.gameObject.transform.position);
+
+        // Calculate direction to shoot the ray
+        Vector3 rayDirection = cameraController.transform.forward;
+        // Cast a debug ray
+        Debug.DrawRay(rayOrigin.position, rayDirection * shootDistance, Color.blue, 1f);
+        // Do the raycast
+        if (Physics.Raycast(rayOrigin.position, rayDirection, out objectHit, shootDistance))
+        {
+            this.gameObject.transform.position = objectHit.point;
+        }
+
+        //wait for seconds??
+        //blinkFinished = true;
+        if (blinkFinished = true)
+        {
+            //landingParticles.Play();
+            //AudioSource.PlayClipAtPoint(landingAudio, this.gameObject.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    private RaycastHit lastRaycastHit;
+    [SerializeField] ParticleSystem handCastParticles;
+    [SerializeField] ParticleSystem blinkPointParticles;
+    [SerializeField] ParticleSystem climbableParticles;
+    [SerializeField] AudioClip startCastAudio;
+    [SerializeField] AudioClip releaseCastAudio;
+
+    private GameObject GetLookedAtObject()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Camera.main.transform.forward;
+        float range = 100;
+        if (Physics.Raycast(origin, direction, out lastRaycastHit, range))
+            return lastRaycastHit.collider.gameObject;
+        else
+            return null;
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void TeleportToLookAt()
+    {
+        transform.position = lastRaycastHit.point + lastRaycastHit.normal * 2;
+        if (startCastAudio != null)
+            AudioSource.PlayClipAtPoint(startCastAudio, transform.position);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            if (GetLookedAtObject() != null)
+                TeleportToLookAt();
+    }
+
+    /*[SerializeField] Camera cameraController;
+    [SerializeField] Transform rayOrigin;
+    CharacterController player;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        player = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cast();
+        }
+    }
+
+    void Cast()
+    {
+        RaycastHit hit;
+
+        Vector3 rayDirection = cameraController.transform.forward;
+        //Vector3 p1 = transform.position + player.center;
+
+        float distanceToObstacle = 0;
+        Debug.DrawRay(rayOrigin.position, rayDirection * distanceToObstacle, Color.blue, 1f);
+
+        if (Physics.Raycast(rayOrigin.position, rayDirection, out hit, distanceToObstacle))
+        {
+            distanceToObstacle = hit.distance;
+            player.transform.position = hit.transform.position;
+        }
+    }*/
+}
